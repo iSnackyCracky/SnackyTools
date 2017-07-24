9f528afa70c05730fabed5f035859a31cc57f767
iSnackyCracky <julian@tueselmann96.de>
2017-07-24 21:46:58 +0200

    add seperate functions to connect ExchangeOnlinePowershell and SharePointOnlinePowershell

---
+++ Functions/Office365/Connect-ExchangeOnline.ps1
@@ -0,0 +1,48 @@
+﻿###############################################################################################################
+# Language     :  PowerShell 4.0
+# Filename     :  Connect-ExchangeOnline.ps1
+# Autor        :  iSnackyCracky (https://isnackycracky.de/ -- https://github.com/isnackycracky/)
+# Description  :  Connect to the Exchange Online powershell
+# Repository   :  https://github.com/isnackycracky/psmodule
+###############################################################################################################
+
+<#
+  .SYNOPSIS
+  Connect to the Exchange Online powershell
+
+  .DESCRIPTION
+  Connect to the Exchange Online powershell
+
+  .EXAMPLE
+  Connect-ExchangeOnline
+#>
+
+function Connect-ExchangeOnline
+{
+  [CmdletBinding()]
+  param(
+    [Parameter(Position=0,Mandatory=$false,HelpMessage="Login-Credentials")]
+    [System.Management.Automation.CredentialAttribute()]$Credential
+  )
+
+  Begin{
+
+  }
+
+  Process{
+    # Get login-credentials, if none were provided as parameter
+    If (!$Credential) {
+      $Credential = Get-Credential
+    }
+
+    # Create the Exchange Online session
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $Credential -Authentication Basic –AllowRedirection
+
+    # Import the created Exchange Online session
+    Import-Module(Import-PSSession $Session -AllowClobber -DisableNameChecking) -Global -DisableNameChecking
+  }
+
+  End{
+
+  }
+}
---
+++ Functions/Office365/Connect-SharePointOnline.ps1
@@ -0,0 +1,50 @@
+﻿###############################################################################################################
+# Language     :  PowerShell 4.0
+# Filename     :  Connect-SharePointOnline.ps1
+# Autor        :  iSnackyCracky (https://isnackycracky.de/ -- https://github.com/isnackycracky/)
+# Description  :  Connect to the SharePointOnline PowerShell
+# Repository   :  https://github.com/isnackycracky/psmodule
+###############################################################################################################
+
+<#
+  .SYNOPSIS
+  Connect to the SharePointOnline PowerShell
+
+  .DESCRIPTION
+  Connect to the SharePointOnline PowerShell
+
+  .EXAMPLE
+  Connect-SharePointOnline -Url "https://contoso.sharepoint.com"
+#>
+
+function Connect-SharePointOnline
+{
+  [CmdletBinding()]
+  param(
+    [Parameter(Position=0,Mandatory=$true,HelpMessage="SharePoint-URL")]
+    [String]$Url,
+    [Parameter(Position=1,Mandatory=$false,HelpMessage="Login-Credentials")]
+    [System.Management.Automation.CredentialAttribute()]$Credential
+  )
+
+  Begin{
+
+  }
+
+  Process{
+    # Get login-credentials, if none were provided as parameter
+    If (!$Credential) {
+      $Credential = Get-Credential
+    }
+
+    # Import the SharePointOnline Module
+    Import-Module "C:\Program Files\SharePoint Online Management Shell\Microsoft.Online.SharePoint.PowerShell\Microsoft.Online.SharePoint.PowerShell.psd1" -Global
+
+    # Connect the MSOnline Service
+    Connect-SPOService -Url $Url -Credential $Credential
+  }
+
+  End{
+
+  }
+}
