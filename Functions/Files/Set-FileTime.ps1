e9fae85578d95c394964b4aab833f72717047947
iSnackyCracky <julian@isnackycracky.de>
2018-08-11 23:30:06 +0200

    add script to modify file timestamps

---
+++ Functions/Files/Set-FileTime.ps1
@@ -0,0 +1,82 @@
+###############################################################################################################
+# Language     :  PowerShell 4.0
+# Filename     :  Set-FileTime.ps1
+# Autor        :  iSnackyCracky (https://isnackycracky.de/ -- https://github.com/isnackycracky/)
+# Description  :  Set timestamps on a file (like the unix touch command)
+# Repository   :  https://github.com/isnackycracky/SnackyTools
+###############################################################################################################
+
+function Set-FileTime{
+    param(
+      [string[]]$paths,
+      [bool]$only_modification = $false,
+      [bool]$only_access = $false
+    );
+  
+    begin {
+      function updateFileSystemInfo([System.IO.FileSystemInfo]$fsInfo) {
+        $datetime = get-date
+        if ( $only_access )
+        {
+           $fsInfo.LastAccessTime = $datetime
+        }
+        elseif ( $only_modification )
+        {
+           $fsInfo.LastWriteTime = $datetime
+        }
+        else
+        {
+           $fsInfo.CreationTime = $datetime
+           $fsInfo.LastWriteTime = $datetime
+           $fsInfo.LastAccessTime = $datetime
+         }
+      }
+     
+      function touchExistingFile($arg) {
+        if ($arg -is [System.IO.FileSystemInfo]) {
+          updateFileSystemInfo($arg)
+        }
+        else {
+          $resolvedPaths = resolve-path $arg
+          foreach ($rpath in $resolvedPaths) {
+            if (test-path -type Container $rpath) {
+              $fsInfo = new-object System.IO.DirectoryInfo($rpath)
+            }
+            else {
+              $fsInfo = new-object System.IO.FileInfo($rpath)
+            }
+            updateFileSystemInfo($fsInfo)
+          }
+        }
+      }
+     
+      function touchNewFile([string]$path) {
+        #$null > $path
+        Set-Content -Path $path -value $null;
+      }
+    }
+   
+    process {
+      if ($_) {
+        if (test-path $_) {
+          touchExistingFile($_)
+        }
+        else {
+          touchNewFile($_)
+        }
+      }
+    }
+   
+    end {
+      if ($paths) {
+        foreach ($path in $paths) {
+          if (test-path $path) {
+            touchExistingFile($path)
+          }
+          else {
+            touchNewFile($path)
+          }
+        }
+      }
+    }
+  }
